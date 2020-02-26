543850aee13d9729be521fcf3f62377def1c9577
Alexandre Gattiker <algattik@users.noreply.github.com>
2020-02-26 21:00:56 +0100

    Update variables.tf

--- 301-deploy-agent-vms/terraform/variables.tf
+++ 301-deploy-agent-vms/terraform/variables.tf
@@ -25,17 +25,29 @@ variable "department" {
 variable "az_devops_url" {
   type = string
   description = "Specify the Azure DevOps url e.g. https://dev.azure.com/myorg"
+  validation {
+    condition     = substr(var.az_devops_url, 0, 8) == "https://"
+    error_message = "The az_devops_url must be a valid URL, starting with https://"
+  }
 }
 
 variable "az_devops_pat" {
   type = string
   description = "Provide a Personal Access Token (PAT) for Azure DevOps. Create it at https://dev.azure.com/[Organization]/_usersSettings/tokens with permission Agent Pools > Read & manage"
+  validation {
+    condition     = length(var.az_devops_pat) > 4
+    error_message = "The az_devops_pat must be a valid Personal Access Token (PAT) for Azure DevOps"
+  }
 }
 
 variable "az_devops_agent_pool" {
   type = string
   description = "Specify the name of the agent pool - must exist before. Create it at https://dev.azure.com/[Organization]/_settings/agentpools"
   default = "pool001"
+  validation {
+    condition     = length(var.az_devops_agent_pool) > 0
+    error_message = "The az_devops_agent_pool must be a valid pool name"
+  }
 }
 
 variable "az_devops_agent_sshkeys" {
@@ -60,4 +72,8 @@ variable "az_devops_agents_per_vm" {
   type = number
   description = "Number of Azure DevOps agents spawned per VM. Agents will be named with a random prefix."
   default = 4
+  validation {
+    condition     = var.az_devops_agents_per_vm > 0
+    error_message = "The az_devops_agents_per_vm must be at least 1"
+  }
 }
